c636779d3043e2a7cd13159a1f6bfa5eca59b9d1
Amir Edin <amiredin@yahoo.com>
2019-03-22 14:57:57 +0300

    add qoutes

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuotesFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
